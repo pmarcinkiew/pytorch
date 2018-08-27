9bfd20ed4898915feedd2be12b32052cf934938b
Arkadiusz Nowak <a.nowak3@partner.samsung.com>
2018-08-27 11:54:28 +0200

    OpenCL pool operator implemented and tested for Mali GPU

---
+++ caffe2/contrib/opencl/operators/pool_op.cc
@@ -0,0 +1,332 @@
+#include "caffe2/core/context.h"
+#include "caffe2/contrib/opencl/context.h"
+#include "caffe2/contrib/opencl/operator.h"
+
+#include "caffe2/contrib/opencl/kernels/utils.h"
+#include "caffe2/utils/math.h"
+
+#include "caffe2/operators/conv_pool_op_base.h"
+#include "caffe2/operators/pool_op.h"
+
+#include <sys/time.h>
+
+namespace caffe2{
+
+static const char *kernelMaxPool1DForwardNCHW = R"CLC(
+    __kernel void K(const __global float *input, __global float *pooled,
+                    const int n_threads, const int channels,
+                    const int input_height,
+                    const int pooled_height,
+                    const int kernel_h,
+                    const int stride_h,
+                    const int pad_t) {
+
+            int n_thread = get_global_id(0);
+
+            if(n_thread < n_threads) {
+              int ph = n_thread % pooled_height;
+              int c = ( n_thread / pooled_height) % channels;
+              int n = n_thread / pooled_height / channels;
+
+              int hstart = ph * stride_h - pad_t;
+              hstart = max(hstart, 0);
+
+              int hend = min(hstart + kernel_h, input_height);
+                            
+              __global const float *input_offset = input + n * channels * input_height;
+              
+              float max_value = -FLT_MAX;
+              int idx = -1;
+              
+              for(int h = hstart; h < hend; ++h) {
+                idx = c * input_height + h;
+                max_value = max(max_value, input_offset[idx]);
+              }
+              pooled[n_thread] = max_value;
+           }
+       }
+)CLC";
+
+static const char *kernelMaxPool2DForwardNCHW = R"CLC(
+   __kernel void K(const __global float *input, __global float *pooled,
+                   const int n_threads, const int channels, 
+                   const int input_width, const int input_height,
+                   const int pooled_width,  const int pooled_height,
+                   const int kernel_w, const int kernel_h,
+                   const int stride_w, const int stride_h,
+                   const int pad_t, const int pad_l ) {
+
+
+          //for (int n_thread  = get_global_id(0); n_thread < n_threads; n_thread+=get_global_size(0)){
+          int n_thread = get_global_id(0);
+
+          if(n_thread < n_threads) {
+            int pw = n_thread % pooled_width;
+            int ph = (n_thread / pooled_width) % pooled_height;
+
+            int c = (n_thread / pooled_width / pooled_height) % channels;
+            int n = n_thread / pooled_width / pooled_height / channels;
+
+            int hstart = ph * stride_h - pad_t;
+            int wstart = pw * stride_w - pad_l;
+
+            int hend = min(hstart + kernel_h, input_height);
+            int wend = min(wstart + kernel_w, input_width);
+
+            hstart = max(hstart, 0);
+            wstart = max(wstart, 0);
+
+
+            __global const float *input_offset = input + n * channels * input_height * input_width;
+
+            float max_value = -FLT_MAX;
+            int idx = -1;
+
+            for (int h = hstart; h < hend; ++h) {
+              for (int w = wstart; w < wend; ++w) {
+                idx = c * input_height * input_width + h * input_width + w;
+                max_value = max( max_value, input_offset[idx] );
+              }
+            }
+            pooled[n_thread] = max_value;
+         }
+      }
+)CLC";
+
+static const char *kernelMaxPool3DForwardNCHW = R"CLC(
+
+    __kernel void K(const __global float *input, __global float *pooled,
+                    const int n_threads, const int channels,
+                    const int input_width, const int input_height, const int input_depth,
+                    const int pooled_width, const int pooled_height, const int pooled_depth,
+                    const int kernel_w, const int kernel_h, const int kernel_d,
+                    const int stride_w, const int stride_h, const int stride_d,
+                    const int pad_t, const int pad_l, const int pad_f) {
+
+
+          int n_thread = get_global_id(0);
+          if(n_thread < n_threads) {
+
+            int pd = n_thread % pooled_depth;
+            int pw = (n_thread / pooled_depth) % pooled_width;
+            int ph = (n_thread / pooled_depth / pooled_width) % pooled_height;
+
+            int c = (n_thread / pooled_depth / pooled_width / pooled_height) % channels;
+            int n = n_thread / pooled_depth / pooled_width / pooled_height / channels;
+
+            int hstart = ph * stride_h - pad_t;
+            int wstart = pw * stride_w - pad_l;
+
+            int hend = min(hstart + kernel_h, input_height);
+            int wend = min(wstart + kernel_w, input_width);
+
+            int dstart = pd * stride_d - pad_f;
+
+            int dend = min(dstart + kernel_d, input_depth);
+
+            hstart = max(hstart, 0);
+            wstart = max(wstart, 0);
+            dstart = max(dstart, 0);
+
+            float max_value = -FLT_MAX;
+
+            __global const float *input_offset = input + n * channels * input_height * input_width * input_depth;
+
+            int idx = -1;
+            for(int h = hstart; h < hend; ++h) {
+              for(int w = wstart; w < wend; ++w) {
+                for(int d = dstart; d < dend; ++d) {
+                  idx = ((c * input_height + h) * input_width + w ) * input_depth + d;
+                  if(input_offset[idx] > max_value) {
+                    max_value = input_offset[idx];
+                  }
+                  //max_value = max( max_value, input_offset[idx] );
+                }
+              }
+            }
+            pooled[n_thread] = max_value;
+         }
+      }
+)CLC";
+
+class MaxPool{};
+
+
+template<> 
+bool PoolOp<float, OpenCLContext, MaxPool>::RunOnDeviceWithOrderNCHW() {
+
+  auto& X = Input(0);
+  auto* Y = Output(0);
+
+  int inputDimSize = X.dims().size()-2;
+  int kernels_size = kernel_.size();
+  int strides_size = stride_.size();
+
+  int samples = X.dim32(0);
+  CAFFE_ENFORCE_GE(samples, 1, "Number of samples must be >= 1.");
+
+  int channels = X.dim32(1);
+  CAFFE_ENFORCE_GE(channels, 1, "Number of channels must be >= 1.");
+
+  CAFFE_ENFORCE_EQ(kernels_size, inputDimSize, "The size of kernels should be the same.");
+  CAFFE_ENFORCE_EQ(strides_size, inputDimSize);
+  CAFFE_ENFORCE_EQ(strides_size, kernels_size);
+
+  ConvPoolOpBase<OpenCLContext>::SetOutputSize(X, Y, X.dim32(1));
+
+  int height = X.dim32(2);
+  int width = kernel_.size() > 1 ? X.dim32(3) : 1;
+  int depth = kernel_.size() > 2 ? X.dim32(4) : 1;
+
+  int pooled_height = Y->dim32(2);
+  int pooled_width = kernel_.size() > 1 ? Y->dim32(3) : 1;
+  int pooled_depth = kernel_.size() > 2 ? Y->dim32(4) : 1;
+
+  int kernel_width = kernel_w();
+  int kernel_height = kernel_h();
+  int kernel_depth = 0;  
+
+  int stride_width = stride_w();
+  int stride_height = stride_h();
+  int stride_depth = 0;
+
+  int pad_top = pad_t();
+  int pad_low  = pad_l();
+  int pad_depth = 0;
+
+  cl::Buffer* bufferX = (cl::Buffer*)X.template data<float>();
+  cl::Buffer* bufferY = (cl::Buffer*)Y->template mutable_data<float>();
+
+  int output_size = Y->size();
+  int kernel_size = kernel_.size();
+
+
+  cl::Kernel kernel;
+
+  switch(kernel_.size()) {
+    case 1:
+      kernel = context_.BuildKernelCached(kernelMaxPool1DForwardNCHW);
+
+      OPENCL_CHECK(kernel.setArg(0, *bufferX));
+      OPENCL_CHECK(kernel.setArg(1, *bufferY));
+
+      OPENCL_CHECK(kernel.setArg(2, output_size));
+      OPENCL_CHECK(kernel.setArg(3, channels));
+
+      OPENCL_CHECK(kernel.setArg(4, height));
+      OPENCL_CHECK(kernel.setArg(5, pooled_height));
+      OPENCL_CHECK(kernel.setArg(6, kernel_height));
+      OPENCL_CHECK(kernel.setArg(7, stride_height));
+      OPENCL_CHECK(kernel.setArg(8, pad_top));
+      break;
+
+    case 2:
+      kernel = context_.BuildKernelCached(kernelMaxPool2DForwardNCHW);
+
+      OPENCL_CHECK(kernel.setArg(0, *bufferX));
+      OPENCL_CHECK(kernel.setArg(1, *bufferY));
+
+      OPENCL_CHECK(kernel.setArg(2, output_size));
+      OPENCL_CHECK(kernel.setArg(3, channels));
+
+      OPENCL_CHECK(kernel.setArg(4, width));
+      OPENCL_CHECK(kernel.setArg(5, height));
+
+      OPENCL_CHECK(kernel.setArg(6, pooled_width));
+      OPENCL_CHECK(kernel.setArg(7, pooled_height));
+
+      OPENCL_CHECK(kernel.setArg(8, kernel_width));
+      OPENCL_CHECK(kernel.setArg(9, kernel_height));
+
+      OPENCL_CHECK(kernel.setArg(10, stride_width));
+      OPENCL_CHECK(kernel.setArg(11, stride_height));
+
+      OPENCL_CHECK(kernel.setArg(12, pad_top));
+      OPENCL_CHECK(kernel.setArg(13, pad_low));
+      break;
+
+    case 3:
+      kernel_depth = kernel_[2];  
+      stride_depth = stride_[2];
+      pad_depth = pads_[2];
+
+      kernel = context_.BuildKernelCached(kernelMaxPool3DForwardNCHW);
+
+      OPENCL_CHECK(kernel.setArg(0, *bufferX));
+      OPENCL_CHECK(kernel.setArg(1, *bufferY));
+
+      OPENCL_CHECK(kernel.setArg(2, output_size));
+      OPENCL_CHECK(kernel.setArg(3, channels));
+
+      OPENCL_CHECK(kernel.setArg(4, width));
+      OPENCL_CHECK(kernel.setArg(5, height));
+      OPENCL_CHECK(kernel.setArg(6, depth));
+
+      OPENCL_CHECK(kernel.setArg(7, pooled_width));
+      OPENCL_CHECK(kernel.setArg(8, pooled_height));
+      OPENCL_CHECK(kernel.setArg(9, pooled_depth));
+
+      OPENCL_CHECK(kernel.setArg(10, kernel_width));
+      OPENCL_CHECK(kernel.setArg(11, kernel_height));
+      OPENCL_CHECK(kernel.setArg(12, kernel_depth));
+
+      OPENCL_CHECK(kernel.setArg(13, stride_width));
+      OPENCL_CHECK(kernel.setArg(14, stride_height));
+      OPENCL_CHECK(kernel.setArg(15, stride_depth));
+
+      OPENCL_CHECK(kernel.setArg(16, pad_top));
+      OPENCL_CHECK(kernel.setArg(17, pad_low));
+      OPENCL_CHECK(kernel.setArg(18, pad_depth));
+      break;
+
+    default:
+      CAFFE_THROW("Unsupported pooling size: ", kernel_.size());
+      return false;
+  }
+
+  auto& ctx = context_.GetSingleton();
+
+  struct timeval tv;
+
+  gettimeofday(&tv, NULL);
+  long long start = tv.tv_sec * 1000000 + tv.tv_usec;
+
+  cl::Event event;
+  OPENCL_CHECK(ctx.queue.enqueueNDRangeKernel(
+                kernel,
+                cl::NullRange,
+                cl::NDRange(output_size),
+                cl::NullRange,
+                NULL,
+                &event));
+  ctx.queue.finish();
+  event.wait();
+
+  gettimeofday(&tv, NULL);
+  long long end = tv.tv_sec * 1000000 + tv.tv_usec;
+
+  std::stringstream outstr;
+  outstr << "PoolOp " << end << " cpu time delta: " << end - start;
+  outstr << " N: " << samples << " C: " << channels;
+  outstr << " k_size: " << kernel_.size() << " outputsize: " << output_size;
+  outstr << " X " << X.size() << " Y " << Y->size();
+  context_.LogProfilingInfo(event, outstr.str());
+
+  return true;
+}
+
+
+template<>
+bool PoolOp<float, OpenCLContext, MaxPool>::RunOnDeviceWithOrderNHWC(){
+
+  auto& X = Input(0);
+  auto* Y = Output(0);
+  ConvPoolOpBase<OpenCLContext>::SetOutputSize(X, Y, X.dim32(1));
+
+  return true;
+}
+
+
+REGISTER_OPENCL_OPERATOR(MaxPool, PoolOp<float, OpenCLContext, MaxPool>);
+}//namespace caffe2
+
---
+++ caffe2/contrib/opencl/operators/pool_op_test_opencl.cc
@@ -0,0 +1,682 @@
+#include<vector>
+#include<array>
+#include<algorithm>
+#include<iterator>
+#include<utility>
+#include<typeinfo>
+#include<type_traits>
+#include<fstream>
+#include<string>
+#include<sstream>
+#include<ios>
+#include<chrono>
+#include<gtest/gtest.h>
+
+#include "caffe2/core/init.h"
+#include "caffe2/core/context.h"
+#include "caffe2/utils/math.h"
+#include "caffe2/operators/pool_op.h"
+#include "caffe2/contrib/opencl/context.h"
+
+CAFFE2_DECLARE_string(caffe_test_root);
+
+namespace caffe2 {
+
+
+template<typename Context> 
+void setDeviceType(OperatorDef& def) {
+  def.mutable_device_option()->set_device_type(CPU);
+}
+
+
+template<>
+void setDeviceType<OpenCLContext>(OperatorDef& def){
+  def.mutable_device_option()->set_device_type(OPENCL);
+}
+
+
+template <typename CtxA, typename CtxB>
+struct CLContext{
+  using Contex = CtxA;
+};
+
+
+template <typename Ctx>
+struct CLContext<OpenCLContext, Ctx> {
+  using Context = OpenCLContext;
+};
+
+
+template<typename Ctx>
+struct CLContext<Ctx, OpenCLContext> {
+  using Context = OpenCLContext;
+};
+
+
+template<typename CtxSrc, typename CtxDst> 
+class TensorToTensor {
+
+  public:
+    static void transfer(Tensor<CtxSrc> &tensorSrc, Tensor<CtxDst> &tensorDst) {
+
+      tensorDst.ResizeLike(tensorSrc);
+
+      DeviceOption option;
+
+      using Ctx = typename CLContext<CtxSrc, CtxDst>::Context;
+
+      Ctx  context(option);
+      context.template Copy<float>(tensorSrc, tensorDst);
+    }
+};
+
+
+template <typename Ctx>
+class TensorToTensor<Ctx, Ctx> {
+  public:
+    static void transfer(Tensor<Ctx> &tensorSrc, Tensor<Ctx> &tensorDst) {
+      tensorDst.ResizeLike(tensorSrc);
+      tensorDst.ShareData(tensorSrc);
+    }
+};
+
+
+struct ShapeConfig {
+  int N;
+  int C;
+  int H;
+  int W;
+  int D;
+};
+
+
+using OperatorLead = std::pair<std::string, TensorCPU&>;
+using OperatorLeads = std::vector<OperatorLead>;
+using OperatorArgs = std::vector<Argument>;
+using OperatorDesc = std::pair<std::string, std::string>;
+
+void showTensorData(TensorCPU &);
+
+
+template <typename Context>
+class OpTest {
+
+  public:
+    OpTest(OperatorDesc &operatorDesc, OperatorLeads &inputLeads, OperatorLeads &outputLeads, OperatorArgs &args) 
+    : m_context(m_option),
+      m_operatorDesc(operatorDesc), m_inputLeads(inputLeads), m_outputLeads(outputLeads), m_args(args)
+    {
+        setDeviceType<Context>(m_opDef);
+    };
+
+  public:
+
+    void operator()() {
+
+      m_opDef.set_name(m_operatorDesc.first);  
+      m_opDef.set_type(m_operatorDesc.second);
+
+      copyDataFromInputTensorsCPU();
+
+      addInputLeads();
+      addOutputLeads();
+      addArguments();
+      auto start = std::chrono::system_clock::now();
+
+      runOperator();
+
+      auto end = std::chrono::system_clock::now();
+      m_elapsed_time = end - start;
+
+      copyDataToOutputTensorsCPU();
+      return;
+    }
+    std::chrono::duration<double> elapsed_time(){ return m_elapsed_time; }
+
+  private:
+    DeviceOption m_option;
+    Context  m_context;
+    Workspace m_ws;
+    OperatorDef m_opDef;
+
+    OperatorDesc& m_operatorDesc;
+    OperatorLeads& m_inputLeads;
+    OperatorLeads& m_outputLeads;
+    OperatorArgs& m_args;
+    std::chrono::duration<double> m_elapsed_time;
+
+
+    void copyDataFromInputTensorsCPU() {
+
+      for(OperatorLead &inputLead: m_inputLeads) {
+        auto inputLeadName = inputLead.first;
+        auto inputTensor = m_ws.CreateBlob(inputLeadName)->GetMutable<Tensor<Context>>();
+        auto inputTensorCPU = inputLead.second;
+
+        TensorToTensor<CPUContext, Context>::transfer(inputTensorCPU, *inputTensor);
+       }
+    }
+
+    void addInputLeads() {
+      for(OperatorLead &inputLead: m_inputLeads) {
+       auto inputLeadName = inputLead.first;
+       m_opDef.add_input(inputLeadName);
+      }
+    }
+
+    void addOutputLeads() {
+      for (OperatorLead &outputLead: m_outputLeads) {
+        auto outputLeadName = outputLead.first;
+        m_ws.CreateBlob(outputLeadName);
+        m_opDef.add_output(outputLeadName);
+      }
+    }
+
+    void addArguments() {
+      for(Argument& arg: m_args) {
+        m_opDef.add_arg()->CopyFrom(arg);
+      }
+    }
+
+    void runOperator() {
+      unique_ptr<OperatorBase> op(CreateOperator(m_opDef, &m_ws));
+
+      op.get();
+      op->Run();
+    }
+
+    void copyDataToOutputTensorsCPU() {
+
+      for (OperatorLead &outputLead: m_outputLeads) {
+        auto outputLeadName = outputLead.first;
+        auto outputTensor = m_ws.GetBlob(outputLeadName)->GetMutable<Tensor<Context>>();
+        auto& outputTensorCPU = outputLead.second;
+
+        TensorToTensor<Context, CPUContext>::transfer(*outputTensor, outputTensorCPU);
+      }
+    }
+};
+
+
+void fillInputTensor(TensorCPU &inputTensor) {
+  auto* data = inputTensor.template mutable_data<float>();
+  int x = 0;
+  for (x=0; x<inputTensor.size(); x++) {
+    data[x] = (float)(x%5);
+  }
+}
+
+
+void zeroInputTensor(TensorCPU &inputTensor) {
+  auto* data = inputTensor.template mutable_data<float>();
+  int x = 0;
+  for (x=0; x<inputTensor.size(); x++) {
+    data[x] = 0.0f;
+  }
+}
+
+
+void showTensorData(TensorCPU &tensor) {
+  const int cN = 0;
+  const int cC = 1;
+  const int cH = 2;
+  const int cW = 3;
+  const int cD = 4;
+
+  int tensor_H = tensor.dim32(cW);
+  std::cout<< "-----------==========" << std::endl;
+  int tensor_W = tensor.dim32(cH);
+  int area_size = tensor_H * tensor_W;
+  int tensor_N = tensor.dim32(cN);
+  int tensor_C = tensor.dim32(cC);
+  std::cout << tensor_C<<std::endl;
+
+  for (int iN = 0; iN < tensor_N; iN++) {
+    std::cout<<"iN:---:"<<iN<<std::endl;
+      for (int iC = 0; iC < tensor_C; iC++) {
+        std::cout<<"iC::"<<iC<<std::endl;
+
+          for(int y = 0; y < tensor_H; y++){
+
+            for (int x = 0; x < tensor_W; x++) {
+
+              int area_idx = iN * tensor_C + iC;
+
+              std::cout<<tensor.data<float>()[   area_idx*(area_size) + (y*tensor_W + x) ]<<" ";
+
+            }
+            std::cout<<std::endl;
+          }
+          std::cout<<std::endl;
+       }
+       std::cout<<std::endl<<std::endl;
+    }
+}
+
+using FFPair = std::pair<float, float>;
+
+
+class TensorsPairedData {
+
+  public:
+    TensorsPairedData() = delete;
+
+    TensorsPairedData(const TensorCPU &tensorCpu, const TensorCPU &tensorCl) {
+      bool tensors_size_are_not_the_same = (tensorCpu.size() != tensorCl.size());
+      bool tensor_size_is_zero = ((tensorCpu.size() == 0) || (tensorCl.size() == 0));
+
+      if (tensors_size_are_not_the_same || tensor_size_is_zero) {
+          compareResult.resize(0);
+          return;
+      }
+      fillPair(tensorCpu, tensorCl);
+    };
+    
+   const std::vector<FFPair>& operator()() const  {return compareResult;}
+
+  private:
+    std::vector<FFPair> compareResult;
+
+    void fillPair(const TensorCPU &tensorCpu, const TensorCPU &tensorCl) {
+
+      const float* cpuData = tensorCpu.data<float>();
+
+      std::vector<float> vCpu(cpuData, cpuData + tensorCpu.size());
+
+      const float* clData = tensorCl.data<float>();
+      std::vector<float> vCl(clData, clData + tensorCl.size());
+
+      std::transform(
+         vCpu.begin(),
+         vCpu.end(),
+         vCl.begin(),
+         std::back_inserter(compareResult),
+         [](float a, float b) {return std::make_pair(a, b); }
+     );
+   }
+};
+
+
+using shapeVector = std::vector<TIndex>;
+
+
+struct AA {
+   int a;
+   int b;
+};
+
+
+
+TEST(OpenCL_and_CPU, MaxPool1DNCHWTest) {
+
+  ShapeConfig sh;
+
+  shapeVector sv{
+    sh.N=3,
+    sh.C=3,
+    sh.H=16
+  };
+
+  TensorCPU tensorX{sv};
+
+  fillInputTensor(tensorX);
+
+  TensorCPU tensorYCpu;
+  TensorCPU tensorYCl;
+
+  OperatorDesc maxPoolDesc{"MaxPoolOp", "MaxPool"};
+  OperatorLeads inLeads{{"inX", tensorX}};
+
+  OperatorLeads outLeadsCPU{{"outY", tensorYCpu}};
+  OperatorLeads outLeadsCL{{"outY", tensorYCl}};
+
+
+  std::vector<Argument> argumentList{
+    MakeArgument("order", std::string("NCHW")),
+    MakeArgument("kernels", std::vector<int>{2}),
+    MakeArgument("strides", std::vector<int>{2})
+  };
+
+  OpTest<CPUContext> maxPoolTestCPU{maxPoolDesc, inLeads, outLeadsCPU, argumentList};
+  maxPoolTestCPU();
+ 
+  OpTest<OpenCLContext> maxPoolTestCL{maxPoolDesc, inLeads, outLeadsCL, argumentList};
+  maxPoolTestCL();
+
+  EXPECT_EQ(tensorYCpu.size(), tensorYCl.size());
+
+  TensorsPairedData tensorsPairedData{tensorYCpu, tensorYCl};
+
+  const std::vector<FFPair>& compareOutput = tensorsPairedData();
+
+  for (FFPair compareFFPair: compareOutput) {
+    SCOPED_TRACE("Cpu value --- OpenCL value");
+    EXPECT_FLOAT_EQ(compareFFPair.first, compareFFPair.second);
+  }
+  SUCCEED();
+}
+
+
+
+TEST(OpenCL_and_CPU, MaxPool2DNCHWTest) {
+
+  ShapeConfig sh;
+
+  shapeVector sv{
+    sh.N=2,
+    sh.C=3,
+    sh.H=16,
+    sh.W=16
+  };
+  
+  TensorCPU tensorX{sv};
+
+  fillInputTensor(tensorX);
+
+  TensorCPU tensorYCpu;
+  TensorCPU tensorYCl;
+
+
+  OperatorDesc maxPoolDesc{"MaxPoolOp", "MaxPool"};
+  OperatorLeads inLeads{{"inX", tensorX}};
+
+  OperatorLeads outLeadsCPU{{"outY", tensorYCpu}};
+  OperatorLeads outLeadsCL{{"outY", tensorYCl}};
+
+  std::vector<Argument> argumentList{
+    MakeArgument("order", std::string("NCHW")),
+    MakeArgument("kernels", std::vector<int>{4,2}),
+    MakeArgument("strides", std::vector<int>{2,2})
+  };
+
+
+  OpTest<CPUContext> maxPoolTestCPU{maxPoolDesc, inLeads, outLeadsCPU, argumentList};
+  maxPoolTestCPU();
+
+  OpTest<OpenCLContext> maxPoolTestCL{maxPoolDesc, inLeads, outLeadsCL, argumentList};
+  maxPoolTestCL();
+
+  EXPECT_EQ(tensorYCpu.size(), tensorYCl.size());
+
+  TensorsPairedData tensorsPairedData{tensorYCpu, tensorYCl};
+
+  const std::vector<FFPair>& compareOutput = tensorsPairedData();
+
+  for (FFPair compareFFPair: compareOutput) {
+    SCOPED_TRACE("Cpu value --- OpenCL value");
+    EXPECT_FLOAT_EQ(compareFFPair.first, compareFFPair.second);
+  }
+  SUCCEED();
+}
+
+
+TEST(OpenCL_and_CPU, MaxPool2DNCHWTestLegacy) {
+
+  ShapeConfig sh;
+
+  shapeVector sv{
+    sh.N=2,
+    sh.C=3,
+    sh.H=16,
+    sh.W=16
+  };
+
+  TensorCPU tensorX{sv};
+
+  fillInputTensor(tensorX);
+
+  TensorCPU tensorYCpu;
+  TensorCPU tensorYCl;
+
+  OperatorDesc maxPoolDesc{"MaxPoolOp", "MaxPool"};
+  OperatorLeads inLeads{{"inX", tensorX}};
+
+  OperatorLeads outLeadsCPU{{"outY", tensorYCpu}};
+  OperatorLeads outLeadsCL{{"outY", tensorYCl}};
+
+  std::vector<Argument> argumentList{
+    MakeArgument("order", std::string("NCHW")),
+    MakeArgument("kernel", 2),
+    MakeArgument("stride", 2)
+  };
+
+
+  OpTest<CPUContext> maxPoolTestCPU{maxPoolDesc, inLeads, outLeadsCPU, argumentList};
+  maxPoolTestCPU();
+ 
+  OpTest<OpenCLContext> maxPoolTestCL{maxPoolDesc, inLeads, outLeadsCL, argumentList};
+  maxPoolTestCL();
+ 
+  EXPECT_EQ(tensorYCpu.size(), tensorYCl.size());
+
+  TensorsPairedData tensorsPairedData{tensorYCpu, tensorYCl};
+
+  const std::vector<FFPair>& compareOutput = tensorsPairedData();
+
+  for (FFPair compareFFPair: compareOutput) {
+    SCOPED_TRACE("Cpu value --- OpenCL value");
+    EXPECT_FLOAT_EQ(compareFFPair.first, compareFFPair.second);
+  }
+  SUCCEED();
+}
+
+
+TEST(OpenCL_and_CPU, MaxPool3DNCHWTest) {
+
+  ShapeConfig sh;
+
+   shapeVector sv{
+    sh.N=3,
+    sh.C=3,
+    sh.H=4,
+    sh.W=4,
+    sh.D=4
+  }; 
+
+  TensorCPU tensorX{sv};
+
+  fillInputTensor(tensorX);
+
+  TensorCPU tensorYCpu;
+  TensorCPU tensorYCl;
+
+
+  OperatorDesc maxPoolDesc{"MaxPoolOp", "MaxPool"};
+  OperatorLeads inLeads{{"inX", tensorX}};
+
+  OperatorLeads outLeadsCPU{{"outY", tensorYCpu}};
+  OperatorLeads outLeadsCL{{"outY", tensorYCl}};
+
+  std::vector<Argument> argumentList{
+     MakeArgument("order", std::string("NCHW")),
+     MakeArgument("kernels", std::vector<int>{2,2,2}),
+     MakeArgument("strides", std::vector<int>{1,2,2})
+   };
+
+
+  OpTest<CPUContext> maxPoolTestCPU{maxPoolDesc, inLeads, outLeadsCPU, argumentList};
+  maxPoolTestCPU();
+
+  OpTest<OpenCLContext> maxPoolTestCL{maxPoolDesc, inLeads, outLeadsCL, argumentList};
+  maxPoolTestCL();
+
+  EXPECT_EQ(tensorYCpu.size(), tensorYCl.size());
+
+  TensorsPairedData tensorsPairedData{tensorYCpu, tensorYCl};
+
+  const std::vector<FFPair>& compareOutput = tensorsPairedData();
+
+  for (FFPair compareFFPair: compareOutput) {
+    SCOPED_TRACE("Cpu value --- OpenCL value");
+    EXPECT_FLOAT_EQ(compareFFPair.first, compareFFPair.second);
+  }
+  SUCCEED();
+}
+
+
+TEST(OpenCL_and_CPU, MaxPool3DNCHWTestOnePointCube) {
+
+  ShapeConfig sh;
+
+  shapeVector sv{
+    sh.N=3,
+    sh.C=3,
+    sh.H=1,
+    sh.W=1,
+    sh.D=1
+  };
+
+  TensorCPU tensorX{sv};
+
+  fillInputTensor(tensorX);
+
+  TensorCPU tensorYCpu;
+  TensorCPU tensorYCl;
+
+  OperatorDesc maxPoolDesc{"MaxPoolOp", "MaxPool"};
+  OperatorLeads inLeads{{"inX", tensorX}};
+
+  OperatorLeads outLeadsCPU{{"outY", tensorYCpu}};
+  OperatorLeads outLeadsCL{{"outY", tensorYCl}};
+
+  std::vector<Argument> argumentList{
+    MakeArgument("order", std::string("NCHW")),
+    MakeArgument("kernels", std::vector<int>{1,1,1}),
+    MakeArgument("strides", std::vector<int>{1,1,1})
+  };
+
+  OpTest<CPUContext> maxPoolTestCPU{maxPoolDesc, inLeads, outLeadsCPU, argumentList};
+  maxPoolTestCPU();
+
+  OpTest<OpenCLContext> maxPoolTestCL{maxPoolDesc, inLeads, outLeadsCL, argumentList};
+  maxPoolTestCL();
+
+  EXPECT_EQ(tensorYCpu.size(), tensorYCl.size());
+
+  TensorsPairedData tensorsPairedData{tensorYCpu, tensorYCl};
+
+  const std::vector<FFPair>& compareOutput = tensorsPairedData();
+
+  for (FFPair compareFFPair: compareOutput) {
+    SCOPED_TRACE("Cpu value --- OpenCL value");
+    EXPECT_FLOAT_EQ(compareFFPair.first, compareFFPair.second);
+  }
+  SUCCEED();
+
+}
+
+
+TEST(OpenCL_and_CPU, MaxPool2DNCHWTest20x12x12) {
+
+  ShapeConfig sh;
+
+  shapeVector sv{
+    sh.N=1,
+    sh.C=20,
+    sh.H=12,
+    sh.W=12,
+  };
+
+  TensorCPU tensorX{sv};
+
+  fillInputTensor(tensorX);
+
+  TensorCPU tensorYCpu;
+  TensorCPU tensorYCl;
+
+  OperatorDesc maxPoolDesc{"MaxPoolOp", "MaxPool"};
+  OperatorLeads inLeads{{"inX", tensorX}};
+
+  OperatorLeads outLeadsCPU{{"outY", tensorYCpu}};
+  OperatorLeads outLeadsCL{{"outY", tensorYCl}};
+
+  std::vector<Argument> argumentList{
+    MakeArgument("order", std::string("NCHW")), 
+    MakeArgument("kernels", std::vector<int>{2,2}),
+    MakeArgument("strides", std::vector<int>{2,2})
+  };
+
+
+  OpTest<CPUContext> maxPoolTestCPU{maxPoolDesc, inLeads, outLeadsCPU, argumentList};
+  maxPoolTestCPU();
+
+  OpTest<OpenCLContext> maxPoolTestCL{maxPoolDesc, inLeads, outLeadsCL, argumentList};
+  maxPoolTestCL();
+
+  EXPECT_EQ(tensorYCpu.size(), tensorYCl.size());
+
+  TensorsPairedData tensorsPairedData{tensorYCpu, tensorYCl};
+
+  const std::vector<FFPair>& compareOutput = tensorsPairedData();
+
+  for (FFPair compareFFPair: compareOutput) {
+    SCOPED_TRACE("Cpu value --- OpenCL value");
+    EXPECT_FLOAT_EQ(compareFFPair.first, compareFFPair.second);
+  }
+  SUCCEED();
+}
+
+
+
+TEST(OpenCL_and_CPU, MaxPool2DNCHWTest50x4x4) {
+
+  ShapeConfig sh;
+
+  shapeVector sv{
+    sh.N=1,
+    sh.C=50,
+    sh.H=4,
+    sh.W=4,
+  };
+
+  TensorCPU tensorX{sv};
+
+  fillInputTensor(tensorX);
+
+  TensorCPU tensorYCpu;
+  TensorCPU tensorYCl;
+
+  OperatorDesc maxPoolDesc{"MaxPoolOp", "MaxPool"};
+  OperatorLeads inLeads{{"inX", tensorX}};
+
+  OperatorLeads outLeadsCPU{{"outY", tensorYCpu}};
+  OperatorLeads outLeadsCL{{"outY", tensorYCl}};
+
+  std::vector<Argument> argumentList{
+    MakeArgument("order", std::string("NCHW")),
+    MakeArgument("kernels", std::vector<int>{2,2}),
+    MakeArgument("strides", std::vector<int>{2,2})
+  };
+
+
+  OpTest<CPUContext> maxPoolTestCPU{maxPoolDesc, inLeads, outLeadsCPU, argumentList};
+  maxPoolTestCPU();
+
+  OpTest<OpenCLContext> maxPoolTestCL{maxPoolDesc, inLeads, outLeadsCL, argumentList};
+  maxPoolTestCL();
+
+
+  EXPECT_EQ(tensorYCpu.size(), tensorYCl.size());
+
+  TensorsPairedData tensorsPairedData{tensorYCpu, tensorYCl};
+
+  const std::vector<FFPair>& compareOutput = tensorsPairedData();
+
+  for (FFPair compareFFPair: compareOutput) {
+    SCOPED_TRACE("Cpu value --- OpenCL value");
+    EXPECT_FLOAT_EQ(compareFFPair.first, compareFFPair.second);
+  }
+  SUCCEED();
+}
+
+
+}// namespace caffe2
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  caffe2::GlobalInit(&argc, &argv);
+  auto result = RUN_ALL_TESTS();
+  google::protobuf::ShutdownProtobufLibrary();
+  return result;
+
+}
