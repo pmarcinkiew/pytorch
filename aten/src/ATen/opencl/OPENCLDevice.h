3e2c58e90b733fb74a233eff22157e12d7c25842
Piotr Marcinkiewicz <p.marcinkiew@samsung.com>
2018-11-30 18:43:02 +0100

    OpenCL - final fixes for compilation with CUDA enabled

--- aten/src/ATen/opencl/OPENCLDevice.h
+++ aten/src/ATen/opencl/OPENCLDevice.h
@@ -11,7 +11,7 @@ inline Device getDeviceFromPtr(void* ptr) {
 /*  struct cudaPointerAttributes attr;
   AT_CUDA_CHECK(cudaPointerGetAttributes(&attr, ptr));
   return {DeviceType::CUDA, attr.device};
-* /
+*/
 }
 
 }} // namespace at::cuda
---
+++ aten/src/ATen/opencl/OPENCLGenerator.cpp
@@ -0,0 +1,56 @@
+#include "ATen/Config.h"
+
+#include "ATen/OPENCLGenerator.h"
+#include "ATen/Context.h"
+#include "THCTensorRandom.h"
+#include <stdexcept>
+
+// There is only one OPENCLGenerator instance. Calls to seed(), manualSeed(),
+// initialSeed(), and unsafeGetTH() refer to the THCGenerator on the current
+// device.
+
+THCGenerator* THCRandom_getGenerator(THCState* state);
+
+namespace at {
+
+OPENCLGenerator::OPENCLGenerator(Context * context_)
+  : context(context_)
+{
+}
+
+OPENCLGenerator::~OPENCLGenerator() {
+  // no-op Generator state is global to the program
+}
+
+OPENCLGenerator& OPENCLGenerator::copy(const Generator& from) {
+  throw std::runtime_error("OPENCLGenerator::copy() not implemented");
+}
+
+OPENCLGenerator& OPENCLGenerator::free() {
+  //THCRandom_shutdown(context->getTHCState());
+  return *this;
+}
+
+uint64_t OPENCLGenerator::seed() {
+  return 0;//THCRandom_initialSeed(context->getTHCState());
+}
+
+uint64_t OPENCLGenerator::initialSeed() {
+  return 0;//THCRandom_initialSeed(context->getTHCState());
+}
+
+OPENCLGenerator& OPENCLGenerator::manualSeed(uint64_t seed) {
+  //THCRandom_manualSeed(context->getTHCState(), seed);
+  return *this;
+}
+
+OPENCLGenerator& OPENCLGenerator::manualSeedAll(uint64_t seed) {
+  //THCRandom_manualSeedAll(context->getTHCState(), seed);
+  return *this;
+}
+
+void * OPENCLGenerator::unsafeGetTH() {
+  return (void*)NULL;//THCRandom_getGenerator(context->getTHCState());
+}
+
+} // namespace at
